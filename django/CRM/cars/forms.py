f6eb4543eed90e7016170c60de941eefa67e61ee
Vitor Rafael <rqfvitor@gmail.com>
2023-07-15 20:03:56 -0300

    refactor: convert unique variables into META

--- django/CRM/cars/forms.py
+++ django/CRM/cars/forms.py
@@ -5,77 +5,55 @@ from .models import Car
 
 
 class CarForm(forms.ModelForm):
-    model = forms.CharField(
-        label="Model",
-        max_length=100,
-        widget=forms.TextInput(attrs={"class": "input input-bordered w-full max-w-xs"}),
-    )
-    brand = forms.CharField(
-        label="Brand",
-        max_length=100,
-        widget=forms.TextInput(attrs={"class": "input input-bordered w-full max-w-xs"}),
-    )
-    year = forms.IntegerField(
-        label="Year",
-        widget=forms.NumberInput(
-            attrs={"class": "input input-bordered w-full max-w-xs"}
-        ),
-    )
-    color = forms.CharField(
-        label="Color",
-        max_length=100,
-        widget=forms.TextInput(attrs={"class": "input input-bordered w-full max-w-xs"}),
-    )
-    plate = forms.CharField(
-        validators=[
-            RegexValidator(
-                regex=r"^([A-Za-z]{3}[-]?\d{4})|([A-Za-z]{4}[-]?\d{3})$",
-                message="Plate must be with format AAA9999, AAAA999, AAA-9999 or AAAA-999",
-                code="invalid_plate",
-            )
-        ],
-        label="Plate",
-        max_length=8,
-        widget=forms.TextInput(attrs={"class": "input input-bordered w-full max-w-xs"}),
-    )
-    chassis_type = forms.CharField(
-        label="Chassis Type",
-        max_length=20,
-        widget=forms.TextInput(attrs={"class": "input input-bordered w-full max-w-xs"}),
-    )
-    image = forms.ImageField(
-        label="Image",
-        required=False,
-        widget=forms.FileInput(attrs={"class": "input input-bordered w-full max-w-xs"}),
-    )
-
+    class Meta:
+        model = Car
+        fields = "__all__"
+        widgets = {
+            "model": forms.TextInput(attrs={"class": "input input-bordered w-full max-w-xs"}),
+            "brand": forms.TextInput(attrs={"class": "input input-bordered w-full max-w-xs"}),
+            "year": forms.NumberInput(attrs={"class": "input input-bordered w-full max-w-xs"}),
+            "color": forms.TextInput(attrs={"class": "input input-bordered w-full max-w-xs"}),
+            "plate": forms.TextInput(attrs={"class": "input input-bordered w-full max-w-xs"}),
+            "chassis_type": forms.TextInput(attrs={"class": "input input-bordered w-full max-w-xs"}),
+            "image": forms.FileInput(attrs={"class": "input input-bordered w-full max-w-xs"}),
+        }
+        labels = {
+            "model": "Model",
+            "brand": "Brand",
+            "year": "Year",
+            "color": "Color",
+            "plate": "Plate",
+            "chassis_type": "Chassis Type",
+            "image": "Image",
+        }
+        error_messages = {
+            "year": {"invalid": "Year must be between 1900 and 2100."},
+            "plate": {
+                "invalid": "Plate must be with format AAA9999, AAAA999, AAA-9999 or AAAA-999."
+            },
+        }
 
     def clean_year(self):
         year = self.cleaned_data.get("year")
-        if year < 1900 or year > 2100:
-            raise forms.ValidationError("Year must be >= 1900 or <= 2100.")
+        if not 1900 <= year <= 2100:
+            raise forms.ValidationError("Year must be between 1900 and 2100.")
         return year
 
     def save_car(self):
-
         plate = self.cleaned_data["plate"].replace("-", "")
-        car = Car(plate=plate.upper())
-        car.model = self.cleaned_data["model"]
-        car.brand = self.cleaned_data["brand"]
-        car.year = self.cleaned_data["year"]
-        car.color = self.cleaned_data["color"]
-        car.chassis_type = self.cleaned_data["chassis_type"]
-        car.branch = self.cleaned_data["branch"]
-        # Acesse o arquivo enviado
+        car = Car(
+            plate=plate.upper(),
+            model=self.cleaned_data["model"],
+            brand=self.cleaned_data["brand"],
+            year=self.cleaned_data["year"],
+            color=self.cleaned_data["color"],
+            chassis_type=self.cleaned_data["chassis_type"],
+            branch=self.cleaned_data["branch"],
+        )
         image_file = self.cleaned_data["image"]
-        
         if image_file:
-            # Atribua o arquivo ao campo 'image'
             car.image.save(image_file.name, image_file)
-        
-        # # Salve o carro
         car.save()
-  
 
     def update_car(self, car):
         plate = self.cleaned_data["plate"].replace("-", "")
@@ -86,9 +64,4 @@ class CarForm(forms.ModelForm):
         car.color = self.cleaned_data["color"]
         car.chassis_type = self.cleaned_data["chassis_type"]
         car.branch = self.cleaned_data["branch"]
-
         car.save()
-
-    class Meta:
-        model = Car
-        fields = "__all__"
